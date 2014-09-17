b92fcba330a0c65278295fa5d1d3f6d53b7141e9
WilliamDevine <williamdevine@mac.com>
2014-09-16 19:02:39 -0700

    fixed dismiss on terms page

---
+++ DropboxDevine/termsViewController.swift
@@ -0,0 +1,39 @@
+//
+//  termsViewController.swift
+//  DropboxDevine
+//
+//  Created by WilliamDevine on 9/16/14.
+//  Copyright (c) 2014 WilliamDevine. All rights reserved.
+//
+
+import UIKit
+
+class termsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func donedismiss(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion:nil)
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
